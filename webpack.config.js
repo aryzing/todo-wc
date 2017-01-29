22496fc3d15f8cce901aa061c2040a12b1d40b52
Eduard Bardají Puig <ebardaji@gmail.com>
2017-01-29 16:59:38 +0100

    fixes babel problems transpiling too much

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,21 @@
 const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   entry: path.resolve(__dirname, 'client', 'index.js'),
   devtool: 'source-map',
   module: {
     rules: [
-      {test: /\.js$/, exclude: /node_modules/, use: "babel-loader"}
+      {test: /\.js$/, exclude: /node_modules/, use: 'babel-loader'}
     ]
   },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      {from: path.resolve(__dirname, 'client', 'index.html')}
+    ])
+  ]
 }
